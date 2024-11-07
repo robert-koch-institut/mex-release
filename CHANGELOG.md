be297418b632d34e5da80009ea9ad01e68f2e7a3
erichesse <eric.hesse3@yahoo.de>
2024-11-07 14:40:09 +0100

    Feature/mx 1619 GitHub actions dry (#57)
    ### Added
    - automate setup commit signing
    
    ### Changes
    - refactore directory structure of mex-release
    
    ### Fixed
    - linting errors

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,14 +8,17 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [Unreleased]
 
 ### Added
+- automate setup commit signing
 
 ### Changes
+- refactore directory structure of mex-release
 
 ### Deprecated
 
 ### Removed
 
 ### Fixed
+- linting errors
 
 ### Security
 
---
+++ mex/release/setup_commit_signing.py
@@ -0,0 +1,37 @@
+import argparse
+import base64
+import os
+from pathlib import Path
+from subprocess import run
+
+from pdm.cli.commands.base import BaseCommand
+from pdm.core import Core
+from pdm.project import Project
+
+
+class SetupCommitSigningCommand(BaseCommand):
+    """Setup commit signing."""
+
+    def handle(self, project: Project, options: argparse.Namespace) -> None:
+        """Execute the setup commit signing command."""
+        ssh_path = Path.home() / ".ssh"
+        private_key = ssh_path / "mex"
+        public_key = ssh_path / "mex.pub"
+        ssh_path.mkdir(mode=0o700, exist_ok=True)
+        with private_key.open("wb") as fh:
+            fh.write(base64.b64decode(os.environ["SIGNING_KEY"]))
+        with public_key.open("wb") as fh:
+            fh.write(base64.b64decode(os.environ["SIGNING_PUB"]))
+        private_key.chmod(0o600)
+        public_key.chmod(0o600)
+        run(["ssh-add", private_key])
+        run(["git", "config", "--local", "user.email", os.environ["MEX_BOT_EMAIL"]])
+        run(["git", "config", "--local", "user.name", os.environ["MEX_BOT_USER"]])
+        run(["git", "config", "--local", "gpg.format", "ssh"])
+        run(["git", "config", "--local", "user.signingkey", public_key])
+        run(["git", "config", "--local", "commit.gpgsign", "true"])
+
+
+def setup_commit_signing(core: Core) -> None:
+    """Register the commit signing command as a pdm command."""
+    core.register_command(SetupCommitSigningCommand, "setup-commit-signing")
